3b558b147ee48c6e5ceb95e98d18879a00b5abd5
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2019-05-04 19:24:37 +0900

    Implement naive mapping function based on keymap defined as a variable in keyboard.cpp

--- app.ino
+++ app.ino
@@ -9,9 +9,6 @@
 
 #include "keyboard.h"
 
-bool orgCapsLockPressed = false;
-bool orgLeftCtrlPressed = false;
-
 class KbdRptParser : public KeyboardReportParser
 {
     // void PrintKey(uint8_t mod, uint8_t key);
@@ -25,61 +22,16 @@ class KbdRptParser : public KeyboardReportParser
 
 void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after)
 {
-  uint8_t change = before ^ after;
-  if (change == 0) {
-    return;
-  }
-
-  MODIFIERKEYS changeMod;
-  *((uint8_t*)&changeMod) = change;
-
-  MODIFIERKEYS afterMod;
-  *((uint8_t*)&afterMod) = after;
-
-  // Swap
-  orgLeftCtrlPressed = afterMod.bmLeftCtrl;
-  afterMod.bmLeftCtrl = orgCapsLockPressed;
-
-  if (changeMod.bmLeftCtrl) {
-    // LeftCtrl (remapping対象)がDown or Upの場合
-
-    if (orgLeftCtrlPressed) {
-      // Down
-      keyboard::reportPress(*(uint8_t*)&afterMod, 0x39);
-    } else {
-      // Up
-      keyboard::reportRelease(*(uint8_t*)&afterMod, 0x39);
-    }
-  } else {
-    keyboard::reportModifier(*(uint8_t*)&afterMod);
-  }
+  keyboard::reportModifier(before, after);
 }
 
 void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
 {
-  if (key == 0x39) {
-    orgCapsLockPressed = true;  // 記録
-    key = 0;  // オリジナルを無効化
-  } else if (orgCapsLockPressed) {
-    MODIFIERKEYS leftCtrl;
-    leftCtrl.bmLeftCtrl = 1;
-    mod |= *(uint8_t*)&leftCtrl;
-  }
-
   keyboard::reportPress(mod, key);
 }
 
 void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key)
 {
-  if (key == 0x39) {
-    orgCapsLockPressed = false;
-    key = 0;  // 無効化
-  } else if (orgCapsLockPressed) {
-    MODIFIERKEYS leftCtrl;
-    leftCtrl.bmLeftCtrl = 1;
-    mod |= *(uint8_t*)&leftCtrl;
-  }
-
   keyboard::reportRelease(mod, key);
 }
 
--- keyboard.cpp
+++ keyboard.cpp
@@ -36,6 +36,26 @@ static const uint8_t _hidReportDescriptor[] PROGMEM = {
 
 keyreport::KeyReport keyReport;
 
+typedef struct {
+  uint8_t mod;
+  uint8_t key;
+} KeyCode;
+typedef struct {
+  KeyCode from;
+  KeyCode to;
+} KeyMap;
+
+#define KEYMAP_SIZE 3
+const KeyMap keymaps[KEYMAP_SIZE] =
+{
+  {{MOD_LEFT_CTRL, 0}, {0, 0x39}},  // left-ctrl to capsLock
+  {{0, 0x39}, {MOD_LEFT_CTRL, 0}},  // capsLock to left-ctrl
+  // {{0, 0x04}, {0, 0x05}},  // 'a' to 'b'  // Not working
+  {{0, 0x05}, {0, 0x04}},  // 'b' to 'a'
+  // {{MOD_LEFT_SHIFT, 0}, {MOD_LEFT_CTRL, 0}},  // Not working
+};
+bool keyPressed[KEYMAP_SIZE];  // TODO: Confirm that this occupies KEYMAP_SIZE bits, but not KEYMAP_SIZE * 8 bits for memory efficiency.
+
 void _sendReport()
 {
   HID().SendReport(2, &keyReport, sizeof(keyreport::KeyReport));
@@ -47,22 +67,88 @@ void keyboard::initKeyboard(void)
   HID().AppendDescriptor(&node);
 
   keyreport::releaseAllKey(&keyReport);
+
+  memset(&keyPressed, 0, sizeof(keyPressed));
 }
 
 void keyboard::reportPress(uint8_t mod, uint8_t key)
 {
+  // FROM key TO key and mod
+  for (int i=0; i<KEYMAP_SIZE; i++) {
+    if (keymaps[i].from.key == key) {
+      key = keymaps[i].to.key;  // This assumes one 'from' key is mapped to at most 1 'to' key. If There are multiple mappings, only the last one affects.
+      keyPressed[i] = true;     // On the other hand, 'from' key can be mapped to multiple 'to' mods.
+    }
+  }
+
+  // TO mod
+  for (int i=0; i<KEYMAP_SIZE; i++) {
+    if (keyPressed[i]) {
+      mod |= keymaps[i].to.mod;
+    }
+  }
+
   keyreport::setKeyPressed(&keyReport, mod, key);
   _sendReport();
 }
 
 void keyboard::reportRelease(uint8_t mod, uint8_t key)
 {
+  // FROM key TO key and mod
+  for (int i=0; i<KEYMAP_SIZE; i++) {
+    if (keymaps[i].from.key == key) {
+      key = keymaps[i].to.key;
+      keyPressed[i] = false;
+    }
+  }
+
+  // TO mod
+  for (int i=0; i<KEYMAP_SIZE; i++) {
+    if (keyPressed[i]) {
+      mod |= keymaps[i].to.mod;
+    }
+  }
+
   keyreport::setKeyReleased(&keyReport, mod, key);
   _sendReport();
 }
 
-void keyboard::reportModifier(uint8_t mod) {
-  keyReport.modifiers = mod;
+void keyboard::reportModifier(uint8_t before, uint8_t after) {
+  uint8_t change = before ^ after;
+  if (change == 0) { return; }
+
+  uint8_t mappedMod = after;
+
+  for (int i=0; i<KEYMAP_SIZE; i++) {
+    if (keymaps[i].from.mod & change) {
+      // keymaps[i].from.mod changes
+
+      // keymaps[i].from.mod's bit is restored from `before`
+      mappedMod = (mappedMod & ~keymaps[i].from.mod) | (before & keymaps[i].from.mod);
+
+      if (after & change) {
+        // keymaps[i].from.mod DOWN
+        keyPressed[i] = true;
+
+        // 'To' key is added. This assumes one 'from' mod is mapped to at most KEY_REPORT_KEYS_NUM 'to' keys. If there are more 'to' keys, they are ignored and end up an error.
+        keyreport::setKeyPressed(&keyReport, mappedMod, keymaps[i].to.key);
+
+        // 'To' mod is added.
+        mappedMod |= keymaps[i].to.mod;
+      } else {
+        // keymaps[i].from.mod UP
+        keyPressed[i] = false;
+
+        // 'To' key is released.
+        keyreport::setKeyReleased(&keyReport, mappedMod, keymaps[i].to.key);
+
+        // 'To' mod is removed
+        mappedMod |= ~keymaps[i].to.mod;
+      }
+    }
+  }
+
+  keyReport.modifiers = mappedMod;
   _sendReport();
 }
 
