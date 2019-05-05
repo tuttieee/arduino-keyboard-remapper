c54459298428d5e5ae81e96a6ea32082b7c8ba09
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2019-05-05 19:45:42 +0900

    Update app.ino to handle keymapping resulting up with no key output

--- app.ino
+++ app.ino
@@ -10,7 +10,7 @@
 #include "keyboard.h"
 #include "keymap.h"
 
-#define KEYMAP_SIZE 5
+#define KEYMAP_SIZE 6
 const keymap::KeyMap keymaps[KEYMAP_SIZE] =
 {
   {{MOD_LEFT_CTRL, 0}, {0, 0x39}},  // left-ctrl to capsLock
@@ -18,6 +18,7 @@ const keymap::KeyMap keymaps[KEYMAP_SIZE] =
   {{0, 0x04}, {0, 0x05}},  // 'a' to 'b'
   {{0, 0x05}, {0, 0x04}},  // 'b' to 'a'
   {{MOD_LEFT_SHIFT, 0}, {MOD_LEFT_CTRL, 0}},  // left-shift to left-ctrl
+  {{MOD_RIGHT_SHIFT, 0x06}, {0, 0x04}},  // R-Shift + 'c' to 'a'
 };
 bool keyPressedFlags[KEYMAP_SIZE];  // TODO: Confirm that this occupies KEYMAP_SIZE bits, but not KEYMAP_SIZE * 8 bits for memory efficiency.
 
@@ -52,14 +53,26 @@ void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after)
 void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key) {
   keymap::onKeyPressed(keymaps, KEYMAP_SIZE, keyPressedFlags, &mod, &key);
 
-  keyboard::reportPress(mod, key);
+  if (key == 0) {
+    if (mod > 0) {
+      keyboard::reportModifier(mod);
+    }
+  } else {
+    keyboard::reportPress(mod, key);
+  }
 }
 
 void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key)
 {
   keymap::onKeyReleased(keymaps, KEYMAP_SIZE, keyPressedFlags, &mod, &key);
 
-  keyboard::reportRelease(mod, key);
+  if (key == 0) {
+    if (mod > 0) {
+      keyboard::reportModifier(mod);
+    }
+  } else {
+    keyboard::reportRelease(mod, key);
+  }
 }
 
 USB     Usb;
